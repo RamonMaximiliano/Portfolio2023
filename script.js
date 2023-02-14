cb2652e10d40c990ab052386fa1f6e68efac66e7
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2023-02-14 00:13:33 -0300

    tech styles

--- script.js
+++ script.js
@@ -12,47 +12,55 @@ https://ramonmaximiliano.github.io/Portfolio/
 /* https://api.github.com/users/RamonMaximiliano */
 
 function profilePicture() {
-
-    fetch("https://api.github.com/users/RamonMaximiliano")
-        .then(function setPicture(profile_data) {
-            return profile_data.json()
-        })
-        .then(function (profile_data) {
-            document.getElementById("Profile_name").innerHTML = profile_data.name;
-            document.getElementById("profile_picture").setAttribute("src", `${profile_data.avatar_url}`)
-            document.getElementById("Profile_picture_Anchor").setAttribute("href", `${profile_data.html_url}`)
-        })
-        .catch(function () {
-            window.alert("Sorry, you are offline, most of the page content will not load!")
-        })
+  fetch("https://api.github.com/users/RamonMaximiliano")
+    .then(function setPicture(profile_data) {
+      return profile_data.json();
+    })
+    .then(function (profile_data) {
+      document.getElementById("Profile_name").innerHTML = profile_data.name;
+      document
+        .getElementById("profile_picture")
+        .setAttribute("src", `${profile_data.avatar_url}`);
+      document
+        .getElementById("Profile_picture_Anchor")
+        .setAttribute("href", `${profile_data.html_url}`);
+    })
+    .catch(function () {
+      window.alert(
+        "Sorry, you are offline, most of the page content will not load!"
+      );
+    });
 }
 
 window.onload = profilePicture();
 
 /* PROFILE DATA FETCH API END*/
 
-
 /* --------------------------------------------------------------------------------------------------- */
-
 
 /* LANGUAGE LEVEL APPEARANCE START*/
 
-let lang_items = document.querySelectorAll(".language-div-item")
+let lang_items = document.querySelectorAll(".language-div-item");
 
 for (item of lang_items) {
-    item.addEventListener("mouseover", function test(e) {
-        document.getElementById(`level-${e.target.id}`).setAttribute("style", "animation: appear;")
-        document.getElementById(`level-${e.target.id}`).setAttribute("style", "opacity:1")
-    })
+  item.addEventListener("mouseover", function test(e) {
+    document
+      .getElementById(`level-${e.target.id}`)
+      .setAttribute("style", "animation: appear;");
+    document
+      .getElementById(`level-${e.target.id}`)
+      .setAttribute("style", "opacity:1");
+  });
 }
 
 for (item of lang_items) {
-    item.addEventListener("mouseout", function test(e) {
-        document.getElementById(`level-${e.target.id}`).setAttribute("style", "opacity:0")
-    })
+  item.addEventListener("mouseout", function test(e) {
+    document
+      .getElementById(`level-${e.target.id}`)
+      .setAttribute("style", "opacity:0");
+  });
 }
 
 /* LANGUAGE LEVEL APPEARANCE END*/
-
 
 /* --------------------------------------------------------------------------------------------------- */
--- translation.js
+++ translation.js
@@ -1,133 +1,143 @@
-
 /* LANGUAGE CHANGER START */
 
 function portugues() {
-    document.getElementById("Ingles").classList.remove("language-changer-button");
-    document.getElementById("Portugues").classList.add("language-changer-button");
+  document.getElementById("Ingles").classList.remove("language-changer-button");
+  document.getElementById("Portugues").classList.add("language-changer-button");
 
-    /*Header translation start*/
-    document.getElementById("Ingles").innerText = "Inglês";
-    document.getElementById("Portugues").innerText = "Português";
-    document.querySelector(".main-title").innerText = "Portfólio";
-    document.getElementById("Sobre").innerText = "Sobre";
-    document.getElementById("Projetos").innerText = "Projetos";
-    document.getElementById("Curriculo").innerText = "Currículo";
-    /*Header translation end*/
+  /*Header translation start*/
+  document.getElementById("Ingles").innerText = "Inglês";
+  document.getElementById("Portugues").innerText = "Português";
+  document.querySelector(".main-title").innerText = "Portfólio";
+  document.getElementById("Sobre").innerText = "Sobre";
+  document.getElementById("Projetos").innerText = "Projetos";
+  document.getElementById("Curriculo").innerText = "Currículo";
+  /*Header translation end*/
 
-    /*Profile translation start*/
-    document.querySelector(".profession").innerText = "Aprendendo Web Dev, a meta é ser dev full stack";
-    document.getElementById("Fone").innerText = "Fone: ";
-    document.getElementById("Endereço").innerText = "Endereço: ";
-    /*Profile translation end*/
+  /*Profile translation start*/
+  document.querySelector(".profession").innerText =
+    "Aprendendo Web Dev, a meta é ser dev full stack";
+  document.getElementById("Fone").innerText = "Fone: ";
+  document.getElementById("Endereço").innerText = "Endereço: ";
+  /*Profile translation end*/
 
-    /*Footer translation start*/
-    document.getElementById("Portfolio-footer").innerText = "Portfólio";
-    document.getElementById("text-footer").innerText = "Desenvolvido por Ramon Maximiliano";
-    /*Footer translation end*/
+  /*Footer translation start*/
+  document.getElementById("Portfolio-footer").innerText = "Portfólio";
+  document.getElementById("text-footer").innerText =
+    "Desenvolvido por Ramon Maximiliano";
+  /*Footer translation end*/
 
-    /*Education translation start*/
-    document.getElementById("Education").innerText = "Formação";
-    document.getElementById("graduation").innerText = "Análise e desenvolvimento de sistemas";
-    document.getElementById("mba-data").innerText = "MBA em Data Science";
-    document.getElementById("mba-data").innerText = "MBA em Data Science";
-    document.getElementById("mba-project").innerText = "MBA em Gestão de projetos";
-    document.getElementById("graduation-text").innerText = "Graduação em analise, desenvolvimento, documentação, teste, implantação e manutenção de sistemas computacionais e softwares.";
-    document.getElementById("mba-data-text").innerText = "Tecnologia para negócios: AI, Data Science e Big Data. Como utilizar as principais tendências em tecnologia para administrar negócios tecnologicamente competitivos.";
-    document.getElementById("mba-project-text").innerText = "Gestão de projetos desde a sua concepção até a implementação, integração entre equipes, prazos, custos, recursos, alinhado com as melhores práticas internacionais.";
-    /*Education translation end*/
+  /*Education translation start*/
+  document.getElementById("Education").innerText = "Formação";
+  document.getElementById("graduation").innerText =
+    "Análise e desenvolvimento de sistemas";
+  document.getElementById("mba-data").innerText = "MBA em Data Science";
+  document.getElementById("mba-data").innerText = "MBA em Data Science";
+  document.getElementById("mba-project").innerText =
+    "MBA em Gestão de projetos";
+  document.getElementById("graduation-text").innerText =
+    "Graduação em analise, desenvolvimento, documentação, teste, implantação e manutenção de sistemas computacionais e softwares.";
+  document.getElementById("mba-data-text").innerText =
+    "Tecnologia para negócios: AI, Data Science e Big Data. Como utilizar as principais tendências em tecnologia para administrar negócios tecnologicamente competitivos.";
+  document.getElementById("mba-project-text").innerText =
+    "Gestão de projetos desde a sua concepção até a implementação, integração entre equipes, prazos, custos, recursos, alinhado com as melhores práticas internacionais.";
+  /*Education translation end*/
 
-    /*Languages translation start*/
-    document.getElementById("Languages").innerText = "Idiomas";
-    document.getElementById("Portuguese").innerText = "Português";
-    document.getElementById("English").innerText = "Inglês";
-    document.getElementById("Spanish").innerText = "Espanhol";
-    document.getElementById("French").innerText = "Francês";
-    document.getElementById("level-Portuguese").innerText = "Nativo";
-    document.getElementById("level-English").innerText = "Fluente";
-    document.getElementById("level-Spanish").innerText = "Fluente";
-    document.getElementById("level-French").innerText = "Fluente";
+  /*Languages translation start*/
+  document.getElementById("Languages").innerText = "Idiomas";
+  document.getElementById("Portuguese").innerText = "Português";
+  document.getElementById("English").innerText = "Inglês";
+  document.getElementById("Spanish").innerText = "Espanhol";
+  document.getElementById("French").innerText = "Francês";
+  document.getElementById("level-Portuguese").innerText = "Nativo";
+  document.getElementById("level-English").innerText = "Fluente";
+  document.getElementById("level-Spanish").innerText = "Fluente";
+  document.getElementById("level-French").innerText = "Fluente";
 
-    /*Languages translation end*/
+  /*Languages translation end*/
 
+  /*Techs translation start*/
+  document.getElementById("AvançadoHTML").innerText = "Avançado";
+  document.getElementById("AvançadoCSS").innerText = "Avançado";
+  document.getElementById("AvançadoJavaScript").innerText = "Avançado";
+  document.getElementById("AvançadoPython").innerText = "Avançado";
+  document.getElementById("AvançadoNode").innerText = "Avançado";
+  document.getElementById("AvançadoReact").innerText = "Avançado";
+  document.getElementById("AvançadoTypeScript").innerText = "Avançado";
+  document.getElementById("AvançadoSQL").innerText = "Avançado";
+  document.getElementById("AvançadoGitHub").innerText = "Avançado";
+  document.getElementById("AvançadoJava").innerText = "Avançado";
 
-    /*Techs translation start*/
-    document.getElementById("AvançadoHTML").innerText = "Avançado";
-    document.getElementById("AvançadoCSS").innerText = "Avançado";
-    document.getElementById("AvançadoJavaScript").innerText = "Avançado";
-    document.getElementById("AvançadoPython").innerText = "Avançado";
-    document.getElementById("AvançadoNode").innerText = "Avançado";
-    document.getElementById("AvançadoReact").innerText = "Avançado";
-    document.getElementById("AvançadoTypeScript").innerText = "Avançado";
-    document.getElementById("AvançadoSQL").innerText = "Avançado";
-    document.getElementById("AvançadoGitHub").innerText = "Avançado";
-    document.getElementById("AvançadoJava").innerText = "Avançado";
-
-
-    /*Techs translation end*/
-
+  /*Techs translation end*/
 }
 
-
-
 function ingles() {
-    document.getElementById("Portugues").classList.remove("language-changer-button")
-    document.getElementById("Ingles").classList.add("language-changer-button")
+  document
+    .getElementById("Portugues")
+    .classList.remove("language-changer-button");
+  document.getElementById("Ingles").classList.add("language-changer-button");
 
-    /*Header translation start*/
-    document.getElementById("Ingles").innerText = "English";
-    document.getElementById("Portugues").innerText = "Portuguese";
-    document.querySelector(".main-title").innerText = "Portfolio";
-    document.getElementById("Sobre").innerText = "About";
-    document.getElementById("Projetos").innerText = "Projects";
-    document.getElementById("Curriculo").innerText = "Resume";
-    /*Header translation end*/
+  /*Header translation start*/
+  document.getElementById("Ingles").innerText = "English";
+  document.getElementById("Portugues").innerText = "Portuguese";
+  document.querySelector(".main-title").innerText = "Portfolio";
+  document.getElementById("Sobre").innerText = "About";
+  document.getElementById("Projetos").innerText = "Projects";
+  document.getElementById("Curriculo").innerText = "Resume";
+  /*Header translation end*/
 
-    /*Profile translation start*/
-    document.querySelector(".profession").innerText = "Learning Web Dev, the goal is to be a full stack dev";
-    document.getElementById("Fone").innerText = "Phone: ";
-    document.getElementById("Endereço").innerText = "Address: ";
-    /*Profile translation end*/
+  /*Profile translation start*/
+  document.querySelector(".profession").innerText =
+    "Learning Web Dev, the goal is to be a full stack dev";
+  document.getElementById("Fone").innerText = "Phone: ";
+  document.getElementById("Endereço").innerText = "Address: ";
+  /*Profile translation end*/
 
-    /*Footer translation start*/
-    document.getElementById("Portfolio-footer").innerText = "Portfolio";
-    document.getElementById("text-footer").innerText = "Developed by Ramon Maximiliano";
-    /*Footer translation end*/
+  /*Footer translation start*/
+  document.getElementById("Portfolio-footer").innerText = "Portfolio";
+  document.getElementById("text-footer").innerText =
+    "Developed by Ramon Maximiliano";
+  /*Footer translation end*/
 
-    /*Education translation start*/
-    document.getElementById("Education").innerText = "Education";
-    document.getElementById("graduation").innerText = "System Analysis and Development";
-    document.getElementById("mba-data").innerText = "MBA in Data Science";
-    document.getElementById("mba-project").innerText = "MBA in Project Management";
-    document.getElementById("graduation-text").innerText = "Degree in analysis, development, documentation, testing, implementation and maintenance of computer systems and software.";
-    document.getElementById("mba-data-text").innerText = "Technology for business: AI, Data Science and Big Data. How to use key trends in technology to run a technologically competitive business.";
-    document.getElementById("mba-project-text").innerText = "Project management from conception to implementation, integration between teams, deadlines, costs, resources, in line with the best international practices.";
-    /*Education translation end*/
+  /*Education translation start*/
+  document.getElementById("Education").innerText = "Education";
+  document.getElementById("graduation").innerText =
+    "System Analysis and Development";
+  document.getElementById("mba-data").innerText = "MBA in Data Science";
+  document.getElementById("mba-project").innerText =
+    "MBA in Project Management";
+  document.getElementById("graduation-text").innerText =
+    "Degree in analysis, development, documentation, testing, implementation and maintenance of computer systems and software.";
+  document.getElementById("mba-data-text").innerText =
+    "Technology for business: AI, Data Science and Big Data. How to use key trends in technology to run a technologically competitive business.";
+  document.getElementById("mba-project-text").innerText =
+    "Project management from conception to implementation, integration between teams, deadlines, costs, resources, in line with the best international practices.";
+  /*Education translation end*/
 
-    /*Languages translation start*/
-    document.getElementById("Languages").innerText = "Languages";
-    document.getElementById("Portuguese").innerText = "Portuguese";
-    document.getElementById("English").innerText = "English";
-    document.getElementById("Spanish").innerText = "Spanish";
-    document.getElementById("French").innerText = "French";
-    document.getElementById("level-Portuguese").innerText = "Native";
-    document.getElementById("level-English").innerText = "Fluent";
-    document.getElementById("level-Spanish").innerText = "Fluent";
-    document.getElementById("level-French").innerText = "Fluent";    /*Languages translation end*/
+  /*Languages translation start*/
+  document.getElementById("Languages").innerText = "Languages";
+  document.getElementById("Portuguese").innerText = "Portuguese";
+  document.getElementById("English").innerText = "English";
+  document.getElementById("Spanish").innerText = "Spanish";
+  document.getElementById("French").innerText = "French";
+  document.getElementById("level-Portuguese").innerText = "Native";
+  document.getElementById("level-English").innerText = "Fluent";
+  document.getElementById("level-Spanish").innerText = "Fluent";
+  document.getElementById("level-French").innerText =
+    "Fluent"; /*Languages translation end*/
 
-    /*Techs translation start*/
-    document.getElementById("AvançadoHTML").innerText = "Advanced";
-    document.getElementById("AvançadoCSS").innerText = "Advanced";
-    document.getElementById("AvançadoJavaScript").innerText = "Advanced";
-    document.getElementById("AvançadoPython").innerText = "Advanced";
-    document.getElementById("AvançadoNode").innerText = "Advanced";
-    document.getElementById("AvançadoReact").innerText = "Advanced";
-    document.getElementById("AvançadoTypeScript").innerText = "Advanced";
-    document.getElementById("AvançadoSQL").innerText = "Advanced";
-    document.getElementById("AvançadoGitHub").innerText = "Advanced";
-    document.getElementById("AvançadoJava").innerText = "Advanced";
+  /*Techs translation start*/
+  document.getElementById("AvançadoHTML").innerText = "Advanced";
+  document.getElementById("AvançadoCSS").innerText = "Advanced";
+  document.getElementById("AvançadoJavaScript").innerText = "Advanced";
+  document.getElementById("AvançadoPython").innerText = "Advanced";
+  document.getElementById("AvançadoNode").innerText = "Advanced";
+  document.getElementById("AvançadoReact").innerText = "Advanced";
+  document.getElementById("AvançadoTypeScript").innerText = "Advanced";
+  document.getElementById("AvançadoSQL").innerText = "Advanced";
+  document.getElementById("AvançadoGitHub").innerText = "Advanced";
+  document.getElementById("AvançadoJava").innerText = "Advanced";
 
-    /*Techs translation end*/
-
+  /*Techs translation end*/
 }
 
 /* LANGUAGE CHANGER END */
